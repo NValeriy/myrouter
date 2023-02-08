aaf25aeb3cf6de813753231e1e4f8118ffb1925a
nv <sablinnwb@gmail.com>
2023-02-08 21:49:15 +0300

    again

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import {createBrowserRouter, RouterProvider} from 'react-router-dom';
-import Root from './routes/root.jsx';
+import Root, {loader as rootLoader} from './routes/root.jsx';
 import ErrorPage from './error-page';
 import Contact from './routes/contact';
 
@@ -10,6 +10,7 @@ const router = createBrowserRouter([
   {
     path: "/", element: <Root />,
     errorElement: <ErrorPage />,
+    loader: rootLoader,
     children: [
       {
           path: "contacts/:contactId",
